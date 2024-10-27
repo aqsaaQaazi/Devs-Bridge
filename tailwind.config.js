630c8a45498d0a0a413c5508af5c91f905229b66
aqsaaQaazi <aqsaakqaazi@gmail.com>
2024-10-28 00:40:48 +0500

    First Commit: Initialized project file, installed Tailwind and restructured files and folders. + configured tailwind.config.js for fonts and grids.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans:['Roboto', 
+          'sans-serif' ,  
+          "Arial",
+          "Helvetica",
+          "Times New Roman",
+          "Georgia",
+          "Verdana",
+          "Trebuchet MS",
+          "Courier New",
+          "Comic Sans MS",
+          "Impact",
+          "Lucida Console",
+          "Palatino Linotype",
+          "Garamond",
+          "Tahoma",
+          "Segoe UI",
+          "Arial Black",
+          "Baskerville",
+          "Book Antiqua",
+          "Futura",
+          "Century Gothic",
+          "Droid Sans"]
+      },
+      gridTemplateColumns: {
+        "70/30" : "70% 28%"
+      }
+    },
   },
   plugins: [],
 }
