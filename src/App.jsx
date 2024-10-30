8e61b4bd6d49efa696e3a5e341bd42913c3f9d30
aqsaaQaazi <aqsaakqaazi@gmail.com>
2024-10-31 01:31:13 +0500

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Navbar from "./components/header";
 import HeroSection from "./components/hero";
 import Navigators from "./components/ui-navigations";
 import JobListings from "./components/Listings";
+import ViewAllBtn from "./components/ViewAllJobsButton";
 
 export default function Wrapper() {
   return (
     <Fragment>
-      {/* <Navbar /> */}
-      {/* <HeroSection /> */}
-      {/* <Navigators /> */}
+      <Navbar />
+      <HeroSection />
+      <Navigators />
       <JobListings />
+      
     </Fragment>
+
+   
   );
 }
